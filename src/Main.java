2e299e0f6e37b11d92413340adef518beb374731
Reshetnikov Konstantin <kostya.reshetnikov.2017@bk.ru>
2023-03-28 22:40:23 +0600

    thirty commit

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Введите ваше число");
         System.out.println("Ваше число равно: ");
+        System.out.println("Ваше число не равно: ");
+        
     }
 }
