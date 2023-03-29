51bc9882a0b5f2b736294d13043c56964aa4cd1e
Reshetnikov Konstantin <kostya.reshetnikov.2017@bk.ru>
2023-03-29 20:12:20 +0600

    1

--- src/Main.java
+++ src/Main.java
@@ -4,8 +4,5 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Введите ваше число");
         System.out.println("Ваше число равно: ");
-        System.out.println("Ваше число не равно: ");
-        System.out.println("Ваше число не мыслемо");
-        
     }
 }
